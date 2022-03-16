2c5e5c82ed39123df5b04d73b658a51551aa4021
Redhead80 <84457227+Redhead80@users.noreply.github.com>
2022-03-16 18:07:02 +0300

    меню

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,23 @@
-(() => {
-    const menu = document.body.querySelectorAll(".menu__link");
-    for (let i = 0; i < menu.length; i++) {
-        menu[i].addEventListener("click", myFunction);
+const menuItem = document.getElementsByClassName('menu__item');
+const menuLink = document.getElementsByClassName('menu__link');
+const itemActive = document.querySelector('.menu_active');
+const arrItem = Array.from(menuItem);
+
+for (let i = 0; i < menuLink.length; i++) {
+    menuLink[i].onclick = function() {
+        if (menuItem[i].querySelector('.menu_sub') !== null && menuItem[i].querySelector('.menu_active') === null) {
+
+            arrItem.forEach((element, index) => {
+                if (element.querySelector('.menu_active')) {
+                    arrItem[index].querySelector('.menu_sub').classList.remove('menu_active');
+                }
+            });
+
+            menuItem[i].querySelector('.menu_sub').classList.add('menu_active');
+            return menuItem[i].href = false;
+        } else if (menuItem[i].querySelector('.menu_sub') !== null && menuItem[i].querySelector('.menu_active') !== null) {
+            menuItem[i].querySelector('.menu_sub').classList.remove('menu_active');
+            return menuItem[i].href = false;
+        }
     }
-    function myFunction() {
-        this.nextElementSibling.onclick = function() {
-            return false;
-        }
-        if (this.nextElementSibling.className === 'menu menu_sub menu_active') {
-            this.nextElementSibling.classList.remove("menu_active");
-        } else {
-            this.nextElementSibling.classList.add("menu_active");
-        }
-    };
-})();
+}
